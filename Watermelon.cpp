7e1719aeb730a1cc91a270494845c1151ebe302f
Anas Jawed <103234658+anasjawed283@users.noreply.github.com>
2023-01-04 23:41:55 +0530

    4A

---
+++ Watermelon.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int weight;
+    cin>>weight;
+    int m;
+    if(weight<=2){
+        cout<<"NO"<<endl;
+    }
+    else{
+        for(int i=2;i<weight;i=i+2){
+        m=weight-i;
+        if(m%2==0){
+            cout<<"YES"<<endl;
+            break;
+        }
+        else{
+            cout<<"NO";
+            break;
+        }
+    }
+    }
+    
+    return 0;
+}
