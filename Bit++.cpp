d29ec075ac5ec20ec50d664e8874f365afcda7d5
Anas Jawed <103234658+anasjawed283@users.noreply.github.com>
2023-01-17 23:42:59 +0530

    282A

---
+++ Bit++.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    string arr[n];
+    int counter=0;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]=="X++"||arr[i]=="++X")
+        counter++;
+        else{
+            counter--;
+        }
+    }
+    cout<<counter<<endl;
+    return 0;
+}
